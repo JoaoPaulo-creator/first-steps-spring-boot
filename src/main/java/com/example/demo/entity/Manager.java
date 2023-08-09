4c74f3e7e58c32886706d889bf22926e73558bcb
JoaoPaulo-creator <joaosarto@gmail.com>
2023-08-08 22:16:27 -0300

    feat: add manager entity

---
+++ src/main/java/com/example/demo/entity/Manager.java
@@ -0,0 +1,60 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "MANAGER")
+public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+
+    @OneToMany(mappedBy = "manager")
+    //@JoinColumn(name = "report_to_id")
+    private Set<Employee> employeeList;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Set<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(Set<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Manager manager)) return false;
+        return Objects.equals(id, manager.id) && Objects.equals(name, manager.name) && Objects.equals(employeeList, manager.employeeList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, employeeList);
+    }
+}
