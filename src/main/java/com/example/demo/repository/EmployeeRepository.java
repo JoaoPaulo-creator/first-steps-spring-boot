820cdd336cd243c264a9bc8953ed9bab43f33d01
JoaoPaulo-creator <joaosarto@gmail.com>
2023-11-18 02:10:56 -0300

    refactor: removido import nao utilizado

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.net.http.HttpHeaders;
 import java.util.UUID;
 
 @Repository
